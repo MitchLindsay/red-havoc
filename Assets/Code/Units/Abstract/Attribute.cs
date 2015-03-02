9349d9c2536ca72346e95846d37c11ff28123e13
Mitch Lindsay <mitch.lindsay@tutanota.com>
2015-03-02 18:57:18 -0500

    Corrected calculations in Attribute.cs. Also added comments to Attribute.cs and AttributeModifier.cs.

--- Assets/Code/Units/Abstract/Attribute.cs
+++ Assets/Code/Units/Abstract/Attribute.cs
@@ -4,8 +4,11 @@ namespace Assets.Code.Units.Abstract
 {
     public class Attribute
     {
+        // Unmodified value of the attribute
         public int BaseValue { get; private set; }
+        // Modified value of the attribute
         public int FinalValue { get; private set; }
+        // List of applied modifiers
         public List<AttributeModifier> Modifiers { get; private set; }
 
         public Attribute(int baseValue)
@@ -15,8 +18,10 @@ namespace Assets.Code.Units.Abstract
             this.Modifiers = new List<AttributeModifier>();
         }
 
+        // Adds a modifier to the list of modifiers
         public void AddModifier(AttributeModifier modifier)
         {
+            // Only add the modifier if it is not already applied
             if (!Modifiers.Contains(modifier))
             {
                 Modifiers.Add(modifier);
@@ -24,8 +29,10 @@ namespace Assets.Code.Units.Abstract
             }
         }
 
+        // Removes a modifier from the list of modifiers
         public void RemoveModifier(AttributeModifier modifier)
         {
+            // Only remove the modifier if it is already applied
             if (Modifiers.Contains(modifier))
             {
                 Modifiers.Remove(modifier);
@@ -33,31 +40,50 @@ namespace Assets.Code.Units.Abstract
             }
         }
 
+        // Removes all modifiers
         public void RemoveAllModifiers()
         {
             Modifiers = new List<AttributeModifier>();
             CalculateFinalValue();
         }
 
+        // Calculates the final value using the list of modifiers
         private void CalculateFinalValue()
         {
-            FinalValue = BaseValue;
+            // Create a temporary value to store the calculations
+            float tempValue = 0.0f;
 
-            foreach (AttributeModifier modifier in Modifiers)
+            // If modifiers exist, apply them
+            if (Modifiers.Count > 0)
             {
-                switch(modifier.ModifierType)
+                // Loop through each modifier in the list and apply them
+                foreach (AttributeModifier modifier in Modifiers)
                 {
-                    case ModifierType.Additive:
-                        FinalValue += (int)(BaseValue + modifier.ModifierValue);
-                        break;
-                    case ModifierType.Multiplicative:
-                        FinalValue += (int)(BaseValue * modifier.ModifierValue);
-                        break;
-                    default:
-                    case ModifierType.None:
-                        FinalValue += 0;
-                        break;
+                    switch (modifier.ModifierType)
+                    {
+                        // Add the modifier to the base value
+                        case ModifierType.Additive:
+                            tempValue += (BaseValue + modifier.ModifierValue);
+                            break;
+                        // Multiply the modifier by the base value
+                        case ModifierType.Multiplicative:
+                            tempValue += (BaseValue * modifier.ModifierValue);
+                            break;
+                        default:
+                        // Do nothing
+                        case ModifierType.None:
+                            tempValue += 0;
+                            break;
+                    }
                 }
+
+                // Convert the temporary value to a whole number, then set it to the final value
+                FinalValue = (int)tempValue;
+            }
+            else
+            {
+                // If there are no modifiers, set final value to the base value of the attribute
+                FinalValue = BaseValue;
             }
         }
     }
--- Assets/Code/Units/Abstract/AttributeModifier.cs
+++ Assets/Code/Units/Abstract/AttributeModifier.cs
@@ -10,7 +10,9 @@ namespace Assets.Code.Units.Abstract
 
     public class AttributeModifier
     {
+        // Type of modifier
         public ModifierType ModifierType { get; private set; }
+        // Value of the modifier to be applied
         public float ModifierValue { get; private set; }
 
         public AttributeModifier(ModifierType modifierType, float modifierValue)
